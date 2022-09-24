54a6516cb5126c8522ca7053e03ae5000fba91e3
aniketgarole <aniketgarole11@gmail.com>
2022-09-24 11:46:24 +0530

    Mathematical operator program

---
+++ mathematical operator/problem2.js
@@ -0,0 +1,14 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four =4;
+let five = 5;
+
+one = one * 2;
+two = two * 2;
+three = three * 2;
+four = four * 2;
+five = five * 2;
+
+
+console.log(one + two + three + four + five);
